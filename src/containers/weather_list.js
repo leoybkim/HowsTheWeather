11ed3e3889e20b9460217096067d167940d4f6bb
leoybkim <leoybkim@gmail.com>
2016-11-17 23:32:09 -0800

    Modularize graph and fetch more weather data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,23 +1,23 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
-
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
 
-    const cityname = cityData.city.name;
-    const citytemp = cityData.list.map(weather => weather.main.temp);
+    const name = cityData.city.name;
+    const temp = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp - 273.15));
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
 
     return (
-      <tr key={cityname}>
-        <td>{cityname}</td>
-        <td>
-          <Sparklines height={120} width={180} data={citytemp}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temp} color="purple" units="C" /></td>
+        <td><Chart data={pressure} color="blue" units="hPa"/></td>
+        <td><Chart data={humidity} color="black" units="%"/></td>
       </tr>
     );
   }
@@ -28,9 +28,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
